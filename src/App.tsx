1088815809657818803be2a82afb94f6ec07cf10
yjlim0428 <yjlim9898@gmail.com>
2022-02-11 11:40:18 +0900

    Test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">test</div>;
+  return <div className="App">test2</div>;
 }
 
 export default App;
