5e28c7ddb1a5800e534e3830211edacaf513dc84
Nix <nix6839@washnix.com>
2022-02-11 10:47:21 +0900

    Remove x-powered-by response header

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.disable('x-powered-by');
+
   await app.listen(3000);
 }
 bootstrap();
