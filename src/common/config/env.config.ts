1ac957a036502420496eb599eeff69c4b49d4b13
Nix <nix6839@washnix.com>
2022-02-11 18:02:00 +0900

    Add typeorm setting

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -5,15 +5,31 @@ import * as Joi from 'joi';
 dotenvExpand.expand(dotenv.config());
 
 interface EnvVars {
-  PORT: number;
   SSL_KEY: string;
   SSL_CERT: string;
+
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+
+  DB_NAME: string;
+  DB_HOST: string;
+  DB_PORT: number;
+
+  PORT: number;
 }
 
 const envSchema = Joi.object<EnvVars, true>({
-  PORT: Joi.number().port().default(3000),
   SSL_KEY: Joi.string().required(),
   SSL_CERT: Joi.string().required(),
+
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+
+  DB_NAME: Joi.string().default('vote_it'),
+  DB_HOST: Joi.string().hostname().default('127.0.0.1'),
+  DB_PORT: Joi.number().port().default(3306),
+
+  PORT: Joi.number().port().default(3000),
 });
 
 const { value: env, error } = envSchema
@@ -24,4 +40,13 @@ if (error !== undefined) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
-export const { PORT, SSL_KEY, SSL_CERT } = env as EnvVars;
+export const {
+  SSL_KEY,
+  SSL_CERT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  PORT,
+} = env as EnvVars;
