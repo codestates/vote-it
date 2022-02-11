519fdff46b5856ab5ea9722c23f6419d4299d153
Nix <nix6839@washnix.com>
2022-02-11 11:29:03 +0900

    Add https settings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SSL_KEY, SSL_CERT } from './common/config/env.config';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const httpsOptions: HttpsOptions = {
+    key: readFileSync(SSL_KEY),
+    cert: readFileSync(SSL_CERT),
+  };
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions,
+  });
   app.disable('x-powered-by');
 
   await app.listen(3000);
