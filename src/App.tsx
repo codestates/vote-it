7dbda0a1a18286f752f400bf7efd882c1d392980
alsqja <alsqha2626@gmail.com>
2022-02-14 16:21:09 +0900

    [Client] fixed import Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import { Header } from './pages/components';
-import { Main } from './pages';
+import { Main } from './pages/Main';
 import { useState } from 'react';
+import { Vote } from './pages/Vote';
 
 // export type LoginProps = boolean;
 
