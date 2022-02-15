dad5768592eb3ade6b0487cd17c816918cc264fb
Nix <72999818+nix6839@users.noreply.github.com>
2022-02-15 13:35:47 +0900

    Update aws application start script (#44)

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo chown -R ubuntu:ubuntu "$HOME/vote-it"
+
 cd "$HOME/vote-it"
 . "$HOME/.nvm/nvm.sh"
+
 npm install
 npm run build
--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 . "$HOME/.nvm/nvm.sh"
+
 pm2 stop main 2> /dev/null || true
 pm2 delete main 2> /dev/null || true
