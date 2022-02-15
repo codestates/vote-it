a711a8c52ae6fdb70648bfcbf713a12595342e9c
Nix <nix6839@washnix.com>
2022-02-15 13:34:51 +0900

    Update aws application start script

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,23 +3,25 @@
 cd "$HOME/vote-it"
 . "$HOME/.nvm/nvm.sh"
 
-function set_node_env_from_aws {
-  VAR_NAME="$1"
-  declare -gx "$VAR_NAME"="$(aws ssm get-parameter --name "/vote-it/$VAR_NAME" --query 'Parameter.Value' | tr -d '"')"
-}
+declare -a envs=(
+  'SSL_KEY'
+  'SSL_CERT'
 
-set_node_env_from_aws 'SSL_KEY'
-set_node_env_from_aws 'SSL_CERT'
+  'JWT_SECRET'
 
-set_node_env_from_aws 'JWT_SECRET'
+  'DB_USERNAME'
+  'DB_PASSWORD'
 
-set_node_env_from_aws 'DB_USERNAME'
-set_node_env_from_aws 'DB_PASSWORD'
+  'DB_NAME'
+  'DB_HOST'
+  'DB_PORT'
 
-set_node_env_from_aws 'DB_NAME'
-set_node_env_from_aws 'DB_HOST'
-set_node_env_from_aws 'DB_PORT'
+  'PORT'
+)
 
-set_node_env_from_aws 'PORT'
+for env in "${envs[@]}"
+do
+  declare -x "$env"="$(aws ssm get-parameter --name "/vote-it/$env" --query 'Parameter.Value' | tr -d '"')"
+done
 
 pm2 start dist/main.js
