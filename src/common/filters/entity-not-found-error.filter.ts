eae7db2e67e60cec78177a8dbff86b825c274775
Nix <72999818+nix6839@users.noreply.github.com>
2022-02-16 10:19:07 +0900

    Refactor some code (#52)
    * NotFound 에러시 발생하는 에러 내용을 통일하기 위해 enum으로 변경하고
      파일로 분리함.
    * updateUserProfileById()에서 성능을 위해 userRepository.update()를
      사용하도록 변경함.
    * updateUserPasswordById()에서 성능을 위해 password만 select하도록
      변경함.

--- src/common/filters/entity-not-found-error.filter.ts
+++ src/common/filters/entity-not-found-error.filter.ts
@@ -1,3 +1,4 @@
+import { NotFoundErrorMessages } from './../not-found-error-messages';
 import {
   ArgumentsHost,
   Catch,
@@ -11,14 +12,14 @@ import { EntityNotFoundError } from 'typeorm';
 export class EntityNotFoundErrorFilter
   implements ExceptionFilter<EntityNotFoundError>
 {
-  private readonly errorMessages: Record<string, string> = {
-    User: '존재하지 않는 유저입니다.',
+  private static readonly errorMessages: Record<string, string> = {
+    User: NotFoundErrorMessages.USER,
   };
 
   catch(exception: EntityNotFoundError, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<Response>();
     const errorEntity = exception.message.split('"')[1];
-    const errorMessage = this.errorMessages[errorEntity];
+    const errorMessage = EntityNotFoundErrorFilter.errorMessages[errorEntity];
     response.status(HttpStatus.NOT_FOUND).json({ message: errorMessage });
   }
 }
---
+++ src/common/not-found-error-messages.ts
@@ -0,0 +1,3 @@
+export const enum NotFoundErrorMessages {
+  USER = '존재하지 않는 유저입니다.',
+}
