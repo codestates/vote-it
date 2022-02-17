4e5335457fb072e346e42a87b5b35adb61834e48
Citysquirrel <86960007+Citysquirrel@users.noreply.github.com>
2022-02-17 15:26:21 +0900

    add: dotenv setting update in webpack config

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './modules';
 
+// import * as dotenv from 'dotenv';
+// dotenv.config({ path: __dirname + '/.env' });
+
 const store = createStore(rootReducer);
 
 ReactDOM.render(
--- src/pages/components/WithdrawalModal.tsx
+++ src/pages/components/WithdrawalModal.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { AiOutlineClose } from 'react-icons/ai';
 import axios from 'axios';
+import apiAxios from '../../utils/apiAxios';
 
 const ModalBackdrop = styled.div`
   position: fixed;
@@ -86,13 +87,11 @@ export const WithdrawalModal = ({ WithdrawalModalHandler }: Iprop) => {
 
   const WithdrawalReq = () => {
     // TODO : 회원 탈퇴 요청 보내기
-    axios.delete(
-      'https://voteit.washnix.com:3000/users/me',
-
-      {
-        withCredentials: true,
-      },
-    );
+    apiAxios
+      .delete(`users/me`, {
+        // delete 요청 바디
+      })
+      .then();
   };
 
   return (
