a4879dfebab5517defd38f932c68ce112ac431c2
alsqja <91926455+alsqja@users.noreply.github.com>
2022-02-21 18:12:24 +0900

    [Client] Fix signup bug (#115)
    * [Client] Edit CreateVote page grid
    
    * [Client] Fix mini sidebar button
    
    * [Client] Add isDark redux
    
    * [Client] Fix darkmode bug
    
    * [Client] Add logo dark mode
    
    * [Client] Edit Header css
    
    * [Client] Add users/me/polls post axios
    
    * [Client] Fix signup bug

--- src/utils/apiAxios.ts
+++ src/utils/apiAxios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'https://localhost:3000';
+const baseURL = 'https://localhost:8000';
 
 const apiAxios = axios.create({
   baseURL,
