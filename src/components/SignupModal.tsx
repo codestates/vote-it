fbf8feb81ed4633c3aceba2f20226957914ff3b6
yjlim0428 <yjlim9898@gmail.com>
2022-02-21 16:24:40 +0900

    [Client] Fix SignupModal CSS
    css수정 및 유효성 검사기능 추가

--- src/components/SignupModal.tsx
+++ src/components/SignupModal.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import apiAxios from '../utils/apiAxios';
 import { useDispatch } from 'react-redux';
 import { notify } from '../modules/notification';
-
+import { RiGoogleFill, RiKakaoTalkFill } from 'react-icons/ri';
 const Canvas = styled.div`
   position: fixed;
   left: 0;
@@ -20,6 +20,7 @@ const Canvas = styled.div`
 
 const View = styled.div`
   /* display: flex; */
+  font-family: 'SUIT-Light';
   flex-direction: column;
   position: absolute;
   left: calc(50vw - var(--modal-width) / 2);
@@ -28,8 +29,9 @@ const View = styled.div`
   width: var(--modal-width);
   background-color: var(--modal-bg);
   border-radius: 16px;
-  border: var(--modal-border);
-  box-shadow: 0 0 8px 8px gray;
+  /* border: var(--modal-border); */
+  box-shadow: -1px -1px 1px var(--box-shadow),
+    2px 2px 6px var(--box-shadow-darker);
   z-index: 999;
   opacity: 0;
   transition: all 0.5s;
@@ -49,6 +51,7 @@ const InputWrapper = styled.div`
   display: flex;
   flex-direction: column;
   /* flex: 1 0 auto; */
+  align-items: center;
   padding: 16px 0;
   margin-bottom: 8px;
   div {
@@ -56,17 +59,18 @@ const InputWrapper = styled.div`
     text-align: left;
   }
   input {
+    font-family: 'SUIT-Light';
     text-align: center;
     font-size: 16px;
     width: 192px;
     height: 24px;
     margin: 16px;
     border: none;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    border-bottom: 2.5px solid rgba(0, 0, 0, 0.2);
 
     :focus {
       outline: none;
-      border-bottom-color: rgba(0, 0, 0);
+      border-bottom-color: var(--main-color);
       transition: all 0.5s;
     }
   }
@@ -79,32 +83,43 @@ const ButtonWrapper = styled.div`
 `;
 
 const Button = styled.button`
-  font-size: 12px;
-  font-weight: bold;
+  font-size: 13px;
+  /* font-weight: bold; */
   width: 192px;
   height: 24px;
   margin-bottom: 8px;
   border: none;
-  border-radius: 8px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: ${(props) => props.color};
   cursor: pointer;
-  box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.4);
+  box-shadow: -1px -1px 1px var(--box-shadow),
+    1px 1px 3px var(--box-shadow-darker);
   :hover {
-    opacity: 0.7;
+    opacity: 0.9;
   }
 `;
 
 const SubWrapper = styled.div`
+  font-family: 'OTWelcomeRA';
   display: flex;
   margin: 8px;
   div {
     margin: 8px;
     cursor: pointer;
     :hover {
-      opacity: 0.7;
+      opacity: 0.6;
     }
   }
   /* flex: 1 0 auto; */
+`;
+export const Warning = styled.div`
+  font-family: 'SUIT-Light';
+  font-size: 12px;
+  margin-top: -10px;
+  color: red;
 `;
 
 interface IModalOn {
@@ -123,6 +138,14 @@ const SignupModal: React.FunctionComponent<IProps> = ({
   setModalOn,
   setModalClass,
 }) => {
+  const [email, setEmail] = useState('');
+  const [isEmail, setIsEmail] = useState(true);
+  const [name, setName] = useState('');
+  const [isName, setIsName] = useState(true);
+  const [password, setPassword] = useState('');
+  const [isPass, setIsPass] = useState(true);
+  const [checkPass, setCheckPass] = useState('');
+  const [isCheck, setIsCheck] = useState(true);
   const [inputValue, setInputValue] = useState({
     email: '',
     password: '',
@@ -143,12 +166,64 @@ const SignupModal: React.FunctionComponent<IProps> = ({
       }
     };
 
-  const handleInputValue =
-    (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInputValue({ ...inputValue, [key]: e.target.value });
+  // const handleInputValue =
+  //   (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     setInputValue({ ...inputValue, [key]: e.target.value });
+  //   };
+  const handleInputValue = () => {
+    setInputValue({ email: email, password: password, nickname: name });
+  };
+
+  const EmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+    const CheckEmail = (email: string): boolean => {
+      return email.includes('@') || email.length === 0;
     };
+    if (!CheckEmail(event.target.value)) {
+      setIsEmail(false);
+    } else {
+      setIsEmail(true);
+    }
+  };
+  const NameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+    const CheckId = (name: string): boolean => {
+      return (name.length <= 14 && name.length >= 4) || name.length === 0;
+    };
+    if (!CheckId(event.target.value)) {
+      setIsName(false);
+    } else {
+      setIsName(true);
+    }
+  };
+  const PasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+    const CheckPass = (password: string): boolean => {
+      return (
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,128}$/.test(
+          password,
+        ) || password.length === 0
+      );
+    };
+    if (!CheckPass(event.target.value)) {
+      setIsPass(false);
+    } else {
+      setIsPass(true);
+    }
+  };
+
+  const CheckPassHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckPass(event.target.value);
+    if (password === event.target.value || password.length === 0) {
+      setIsCheck(true);
+    } else {
+      setIsCheck(false);
+    }
+  };
 
   const SignupHandler = () => {
+    handleInputValue();
+    console.log(inputValue);
     apiAxios
       .post('auth/signup', inputValue)
       .then((res) => {
@@ -181,27 +256,82 @@ const SignupModal: React.FunctionComponent<IProps> = ({
             <input
               type="email"
               placeholder="E-mail"
-              onChange={handleInputValue('email')}
+              value={email}
+              onChange={EmailHandler}
+              // onChange={handleInputValue('email')}
             />
+            <Warning style={isEmail ? { display: 'none' } : {}}>
+              이메일 형식으로 입력해주세요.
+            </Warning>
             <input
               type="text"
               placeholder="nickname"
-              onChange={handleInputValue('nickname')}
+              value={name}
+              onChange={NameHandler}
+              // onChange={handleInputValue('nickname')}
             />
+            <Warning style={isName ? { display: 'none' } : {}}>
+              닉네임은 4~14글자 입니다.
+            </Warning>
             {/* <div>비밀번호</div> */}
             <input
               type="password"
               placeholder="Password"
-              onChange={handleInputValue('password')}
+              value={password}
+              onChange={PasswordHandler}
+              // onChange={handleInputValue('password')}
             />
+            <Warning style={isPass ? { display: 'none' } : {}}>
+              8 ~ 128자의 영문 대소문자, 숫자, 특수문자만 사용 가능합니다.
+            </Warning>
+            <input
+              type="password"
+              placeholder="Password"
+              value={checkPass}
+              onChange={CheckPassHandler}
+              // onChange={handleInputValue('password')}
+            />
+            <Warning style={isCheck ? { display: 'none' } : {}}>
+              비밀번호가 일치하지 않습니다.
+            </Warning>
           </InputWrapper>
           <ButtonWrapper>
-            <Button color="rgb(199,199,199)" onClick={SignupHandler}>
+            <Button
+              style={{ color: 'white' }}
+              color="var(--main-color)"
+              onClick={SignupHandler}
+            >
               이메일로 계정 만들기
             </Button>
-            <Button color="rgb(234,067,053)">구글 로그인</Button>
-            <Button color="rgb(45, 180, 0)">네이버 로그인</Button>
-            <Button color="rgb(249, 224, 0)">카카오 로그인</Button>
+            <Button color="white">
+              <RiGoogleFill
+                style={{ marginRight: '10px', fontSize: '17px', color: 'red' }}
+              />
+              구글 로그인
+            </Button>
+            <Button color="white">
+              <span
+                style={{
+                  marginRight: '5px',
+                  color: '#19CE60',
+                  fontSize: '15px',
+                  fontFamily: 'KOHIBaeumOTF',
+                }}
+              >
+                N
+              </span>
+              네이버 로그인
+            </Button>
+            <Button color="white">
+              <RiKakaoTalkFill
+                style={{
+                  marginRight: '5px',
+                  fontSize: '17px',
+                  color: '#4e4600',
+                }}
+              />
+              카카오 로그인
+            </Button>
           </ButtonWrapper>
           <SubWrapper>
             <div onClick={handleClick('login')}>이미 계정이 있나요? 로그인</div>
