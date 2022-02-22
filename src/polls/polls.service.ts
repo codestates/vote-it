d78f7a820a5b1a17491eecbbe5161fc37815d75e
Nix <nix6839@washnix.com>
2022-02-22 17:16:44 +0900

    Add delete my poll api

--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -1,7 +1,7 @@
 import { UserRepository } from '../users/repositories/user.repository';
 import { Repository } from 'typeorm';
 import { CreatePollDto } from './dto/create-poll.dto';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Poll } from './entities/poll.entity';
 
@@ -20,5 +20,19 @@ export class PollsService {
       ...createPollDto,
     });
     return { pollId: insertResult.raw.insertId as number };
+  }
+
+  async deletePollOfAuthor(pollId: number, authorId: number): Promise<void> {
+    const poll = await this.pollRepository
+      .createQueryBuilder('poll')
+      .select([`poll.id`, 'author.id'])
+      .leftJoin('poll.author', 'author')
+      .where('poll.id = :pollId', { pollId })
+      .andWhere(`author.id = :authorId`, { authorId })
+      .getOneOrFail();
+    if (poll.author.id !== authorId) {
+      throw new ForbiddenException('자신의 투표가 아닙니다.');
+    }
+    this.pollRepository.delete(pollId);
   }
 }
