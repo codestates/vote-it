70f894dbad06a2813e4e9c5b9ec02038501f9ef2
Nix <nix6839@washnix.com>
2022-02-24 15:59:37 +0900

    Use transform instead of type in pagination dto

--- src/common/dto/pagination-query.dto.ts
+++ src/common/dto/pagination-query.dto.ts
@@ -1,16 +1,22 @@
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 import { IsInt, IsOptional, IsPositive, Max, Min } from 'class-validator';
+
+function stringToNumeric(value: string) {
+  const valueIsNumeric = /^-?\d+$/.test(value);
+  return valueIsNumeric ? parseInt(value, 10) : value;
+}
 
 export class PaginationQueryDto {
   @IsInt()
   @Min(0)
   @IsOptional()
-  @Type(() => Number)
+  @Transform(({ value }) => stringToNumeric(value))
   offset = 0;
 
   @IsInt()
   @IsPositive()
   @Max(30)
+  @Transform(({ value }) => stringToNumeric(value))
   @Type(() => Number)
   limit: number;
 }
