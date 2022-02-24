c6108e4299d025789909fb25ce68fdc07db7b1b1
Nix <72999818+nix6839@users.noreply.github.com>
2022-02-24 11:57:51 +0900

    Use typeorm query builder only (#158)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,3 @@
-import { pickUserData } from '../common/utils/pick-data.util';
 import { JwtPayload } from './payloads/jwt.payload';
 import { UserRepository } from '../users/repositories/user.repository';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -20,15 +19,18 @@ export class AuthService {
   }
 
   async login({ email, password }: LoginUserDto) {
-    const user = await this.userRepository.findOne({ email });
-    if (user === undefined || user.password !== password) {
-      throw new ForbiddenException('이메일 또는 비밀번호가 다릅니다.');
+    const { password: encryptedPassword, ...user } = await this.userRepository
+      .createQueryBuilder('user')
+      .select()
+      .where('user.email = :email', { email })
+      .getOneOrFail();
+
+    if (encryptedPassword !== password) {
+      throw new ForbiddenException('잘못된 비밀번호입니다.');
     }
 
     const payload: JwtPayload = { sub: user.id, email };
-    return {
-      accessToken: this.jwtService.sign(payload),
-      user: pickUserData(user),
-    };
+    const accessToken = this.jwtService.sign(payload);
+    return { accessToken, user };
   }
 }
