f35a16b57974408c8f86949d8b951c031a8e00b7
Nix <72999818+nix6839@users.noreply.github.com>
2022-02-28 18:24:08 +0900

    Refactor get db version only first time (#201)

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
+import { version as nodeVersion } from 'process';
 import env from './common/config/env.config';
 
 @Injectable()
 export class AppService {
+  private dbVersion: string | null = null;
+
   constructor(private readonly connection: Connection) {}
 
   async index(): Promise<string> {
     if (env.NODE_ENV !== 'development') {
       return 'Hello, Todo It!';
     }
-    const nodeVersion = process.version;
-    const [{ dbVersion }] = await this.connection
-      .createQueryRunner('master')
-      .query('SELECT VERSION() as dbVersion');
+    if (this.dbVersion === null) {
+      const [{ dbVersion }] = await this.connection
+        .createQueryRunner('master')
+        .query('SELECT VERSION() as dbVersion');
+      this.dbVersion = dbVersion;
+    }
     const ret = `
       <!DOCTYPE html>
       <h1>Debug Information</h1>
       <div>
         <p>Node.js Version: <code>${nodeVersion}</code></p>
-        <p>DB Version: <code>${dbVersion}</code></p>
+        <p>DB Version: <code>${this.dbVersion}</code></p>
       </div>
     `;
     return ret;
