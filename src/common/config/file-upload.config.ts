7773e757b92dc969318df39f5b9469cd3a1dd29f
Nix <nix6839@washnix.com>
2022-03-02 16:14:19 +0900

    Update about poll picture
    * `Poll`entity에 `picture` column 추가
    * POST `/upload-poll-picture` API 추가
    * GET `/polls`, GET `/polls/:pollId`,
      POST `/users/me/polls/:pollId` API들에 `picture` 관련 추가

---
+++ src/common/config/file-upload.config.ts
@@ -0,0 +1,5 @@
+import env from './env.config';
+
+const host = env.NODE_ENV === 'production' ? 'voteit.washnix.com' : '127.0.0.1';
+
+export const POLL_PICTURE_URL = `https://${host}:${env.PORT}/media/poll`;
