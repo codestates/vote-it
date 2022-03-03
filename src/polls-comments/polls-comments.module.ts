5f29b738742500b7d17abe4cb7e40a1547d2a3a8
Nix <nix6839@washnix.com>
2022-03-03 21:06:39 +0900

    Add create poll comment api

--- src/polls-comments/polls-comments.module.ts
+++ src/polls-comments/polls-comments.module.ts
@@ -3,9 +3,10 @@ import { PollsCommentsService } from './polls-comments.service';
 import { PollsCommentsController } from './polls-comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PollComment } from './entities/poll-comment.entity';
+import { Poll } from '../polls/entities/poll.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PollComment])],
+  imports: [TypeOrmModule.forFeature([PollComment, Poll])],
   providers: [PollsCommentsService],
   controllers: [PollsCommentsController],
 })
