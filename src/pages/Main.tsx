b4d99389ec3ec62b3b27d9de9238eb59f9969bf8
yjlim0428 <yjlim9898@gmail.com>
2022-03-04 13:47:00 +0900

    [Client] Edit Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -137,10 +137,7 @@ export const Main = () => {
   };
 
   useEffect(() => {
-    const watch = () => {
-      window.addEventListener('scroll', handleFollow);
-    };
-    watch(); //addEventListener 함수 실행
+    window.addEventListener('scroll', handleFollow);
     return () => {
       window.removeEventListener('scroll', handleFollow);
     };
@@ -181,7 +178,6 @@ export const Main = () => {
           </div>
         )}
       </MainOuter>
-      {isLoading && <LoadingVoteCard />}
     </div>
   );
 };
